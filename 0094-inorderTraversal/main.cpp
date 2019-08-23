c9eab8370d126751d36118df9df70eb6cafaf1e2
MappleTT <1653516704@qq.com>
2019-08-23 15:14:19 +0800

    0094 solved

---
+++ 0094-inorderTraversal/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        while(root != NULL || !data.empty()){
+            while(root != NULL){
+                data.push(root);
+                root=root->left;
+            }
+            if(!data.empty()){
+                TreeNode *node = data.top();
+                data.pop();
+                arr.push_back(node->val);
+                root=node->right;
+            }
+        }
+        return arr;
+    }
+private:
+    stack<TreeNode *>data;
+    vector<int>arr;
+};
