87e4ea361ade9f0a8c7e3cc2a01bb7bf3b073d3c
MappleTT <1653516704@qq.com>
2019-08-23 16:03:51 +0800

    0509 solved

---
+++ 0509-fib/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int N) {
+        arr[0]=0,arr[1]=1;
+        if(N<=1)
+            return arr[N];
+        for(int i=2;i<=N;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[N];
+    }
+private:
+    int arr[31];
+};
+
