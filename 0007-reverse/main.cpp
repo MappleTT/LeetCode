3bf479457878fc3455234da8dc432b940c2ff38d
MappleTT <1653516704@qq.com>
2019-08-23 14:34:42 +0800

    0007 solved

---
+++ 0007-reverse/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        if(x==0)
+            return 0;
+        long long  sum=0;
+        while(x!=0)
+        {
+            sum=sum*10+(x%10);
+            x/=10;
+        }
+        if(sum>INT_MAX||sum<INT_MIN)
+            return 0;
+        else
+            return sum;
+
+    }
+};
+
